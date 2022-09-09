2f4f53c24ca0d7a841ea0b3fe4de67b6b96a074e
ny <dkzndk1100@naver.com>
2022-09-09 12:00:00 +0900

    fix : OAuthException 형식 변경
    - SetDescription()을 이용하여 enum 메시지 형식을 변경하기 보다는 필요한
    메시지가 있다면 추가하는 형식으로 사용하기로 결정.

--- src/main/java/com/example/demo/common/exception/code/OAuthErrorCode.java
+++ src/main/java/com/example/demo/common/exception/code/OAuthErrorCode.java
@@ -12,9 +12,4 @@ public enum OAuthErrorCode {
     EMAIL_ALREADY_SIGNED_UP("이미 가입되어있는 이메일입니다."),
     INVALID_PROVIDER("지원하지 않는 OAuth Provider 입니다.");
     private String description;
-
-    public OAuthErrorCode setDescription(String message) {
-        this.description = message;
-        return this;
-    }
 }
