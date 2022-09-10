6f162e12da266003c5ad8b0b7808c698608d2775
Hanjoon Kim <hanjoondev@gmail.com>
2022-09-10 09:48:40 -0700

    Add admin/upload endpoint, minor refactoring
    prerequisites:
    - create the parent directory
      sudo mkdir -p /opt/zerogift/upload
    - change ownership:group of the parent directory
      sudo chown -R tomcat9:tomcat9 /opt/zerogift
      (same as $CATALINA_HOME/webapps directory)

---
+++ src/main/java/com/example/demo/product/controller/ProductImageController.java
@@ -0,0 +1,47 @@
+package com.example.demo.product.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.common.dto.Result;
+import com.example.demo.common.exception.code.ProductErrorCode;
+import com.example.demo.product.service.ProductImageService;
+import com.example.demo.security.dto.AdminInfo;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductImageController {
+    private final ProductImageService productImageService;
+
+    @PostMapping("admin/upload")
+    public ResponseEntity<Result<?>> uploadImages(
+            @RequestParam("files") MultipartFile[] request) {
+        String email = getEmail();
+        if (email == null) return getResponse(403, ProductErrorCode.INSUFFICIENT_AUTHORITY);
+        return ResponseEntity.ok().body(
+            Result.builder().data(productImageService.upload(request)).build());
+    }
+
+    private ResponseEntity<Result<?>> getResponse(int status, ProductErrorCode errorCode) {
+        return ResponseEntity.badRequest().body(
+                Result.builder().status(status).success(false).data(errorCode).build());
+    }
+
+    private String getEmail() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = null;
+        try {
+            email = ((AdminInfo) auth.getPrincipal()).getEmail();
+        } catch (Exception e) {
+            return email;
+        }
+        return email;
+    }
+}
