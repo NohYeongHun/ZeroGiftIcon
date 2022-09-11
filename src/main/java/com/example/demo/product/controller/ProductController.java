5479c00e9509550455d31d3e0463965f07245047
Hanjoon Kim <hanjoondev@gmail.com>
2022-09-11 03:49:48 -0700

    Refactor getEmail() moved to util.TokenUtil from controllers

--- src/main/java/com/example/demo/product/controller/ProductController.java
+++ src/main/java/com/example/demo/product/controller/ProductController.java
@@ -5,9 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +17,7 @@ import com.example.demo.common.exception.code.ProductErrorCode;
 import com.example.demo.product.dto.NewProductRequest;
 import com.example.demo.product.service.ProductService;
 import com.example.demo.product.type.Category;
-import com.example.demo.security.dto.AdminInfo;
+import com.example.demo.util.TokenUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -32,7 +29,7 @@ public class ProductController {
     @PostMapping("admin/product")
     // @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Result<?>> addProduct(@RequestBody @Valid NewProductRequest request) {
-        String email = getEmail();
+        String email = TokenUtil.getAdminEmail();
         if (email == null) return badRequest(403, ProductErrorCode.INSUFFICIENT_AUTHORITY);
         return ResponseEntity.ok().body(
                 Result.builder().data(productService.addProduct(request, email)).build());
@@ -62,16 +59,5 @@ public class ProductController {
     private ResponseEntity<Result<?>> badRequest(int status, ProductErrorCode errorCode) {
         return ResponseEntity.badRequest().body(
                 Result.builder().status(status).success(false).data(errorCode).build());
-    }
-
-    private String getEmail() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = null;
-        try {
-            email = ((AdminInfo) auth.getPrincipal()).getEmail();
-        } catch (Exception e) {
-            return email;
-        }
-        return email;
     }
 }
