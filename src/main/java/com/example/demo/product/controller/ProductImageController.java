e105d41eaf5f6fdcc5a265c8c0f424025dbf841e
Hanjoon Kim <hanjoondev@gmail.com>
2022-09-11 03:49:48 -0700

    Refactor getEmail() moved to util.TokenUtil from controllers

--- src/main/java/com/example/demo/product/controller/ProductImageController.java
+++ src/main/java/com/example/demo/product/controller/ProductImageController.java
@@ -1,8 +1,6 @@
 package com.example.demo.product.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.demo.common.dto.Result;
 import com.example.demo.common.exception.code.ProductErrorCode;
 import com.example.demo.product.service.ProductImageService;
-import com.example.demo.security.dto.AdminInfo;
+import com.example.demo.util.TokenUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,7 +23,7 @@ public class ProductImageController {
     @PostMapping("admin/upload")
     public ResponseEntity<Result<?>> uploadImages(
             @RequestParam("files") MultipartFile[] request) {
-        String email = getEmail();
+        String email = TokenUtil.getAdminEmail();
         if (email == null) return getResponse(403, ProductErrorCode.INSUFFICIENT_AUTHORITY);
         return ResponseEntity.ok().body(
             Result.builder().data(productImageService.upload(request)).build());
@@ -47,16 +45,5 @@ public class ProductImageController {
     private ResponseEntity<Result<?>> getResponse(int status, ProductErrorCode errorCode) {
         return ResponseEntity.badRequest().body(
                 Result.builder().status(status).success(false).data(errorCode).build());
-    }
-
-    private String getEmail() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = null;
-        try {
-            email = ((AdminInfo) auth.getPrincipal()).getEmail();
-        } catch (Exception e) {
-            return email;
-        }
-        return email;
     }
 }
