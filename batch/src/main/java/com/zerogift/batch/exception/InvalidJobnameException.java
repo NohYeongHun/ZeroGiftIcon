04122efaa10eadbcf39c8cb539309f1a87d0c91a
Shin seongsu <tlstjdtn321@naver.com>
2022-09-13 00:01:35 +0900

    feat: 커스텀 예외 추가

---
+++ batch/src/main/java/com/zerogift/batch/exception/InvalidJobnameException.java
@@ -0,0 +1,8 @@
+package com.zerogift.batch.exception;
+
+public class InvalidJobnameException extends RuntimeException {
+
+    public InvalidJobnameException(String message) {
+        super(message);
+    }
+}
