42e1b9816f7a01b60a71a86af083a5652895c2b9
ny <dkzndk1100@naver.com>
2022-09-17 20:44:54 +0900

    feat : Member 등록 및 로그인 기능 추가
    - Member 등록 및 로그인 기능 추가

---
+++ backend/src/main/java/com/zerogift/backend/member/service/MemberLoginService.java
@@ -0,0 +1,151 @@
+package com.zerogift.backend.member.service;
+
+import com.zerogift.backend.common.exception.EmailAuthException;
+import com.zerogift.backend.common.exception.MemberException;
+import com.zerogift.backend.common.exception.code.EmailAuthErrorCode;
+import com.zerogift.backend.common.exception.code.MemberErrorCode;
+import com.zerogift.backend.common.type.AuthType;
+import com.zerogift.backend.email.dto.EmailAuthRequest;
+import com.zerogift.backend.email.entity.EmailAuth;
+import com.zerogift.backend.email.repository.EmailAuthRepository;
+import com.zerogift.backend.member.dto.MemberLoginRequest;
+import com.zerogift.backend.member.dto.MemberRegisterRequest;
+import com.zerogift.backend.member.entity.Member;
+import com.zerogift.backend.member.repository.MemberRepository;
+import com.zerogift.backend.member.type.MemberStatus;
+import com.zerogift.backend.security.dto.MemberInfo;
+import com.zerogift.backend.security.dto.TokenDto;
+import com.zerogift.backend.security.repository.RefreshTokenRepository;
+import com.zerogift.backend.security.service.TokenService;
+import com.zerogift.backend.security.type.Role;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class MemberLoginService {
+
+    private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final EmailAuthRepository emailAuthRepository;
+    private final TokenService tokenService;
+    private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender javaMailSender;
+
+    @Transactional
+    public TokenDto login(MemberLoginRequest request) {
+        Member member = memberRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+        validateLoginMember(member, request.getPassword());
+
+        return tokenService.issueAllToken(
+                MemberInfo.of(
+                        MemberInfo.builder()
+                                .email(member.getEmail())
+                                .role(member.getRole().name())
+                                .build().toClaims()
+                ));
+    }
+
+    private void validateLoginMember(Member member, String password) {
+        if (!passwordEncoder.matches(password, member.getPassword())){
+            throw new MemberException(MemberErrorCode.PASSWORD_WRONG);
+        }
+
+        if (member.getStatus().equals(MemberStatus.WAIT)){
+            sendEmail(member.getEmail());
+            throw new MemberException(MemberErrorCode.EMAIL_VERIFICATION_HAS_NOT_BEEN_COMPLETED);
+        }
+
+        if (member.getStatus().equals(MemberStatus.BANNED)){
+            throw new MemberException(MemberErrorCode.SUSPEND_USER);
+        }
+
+        if (member.getStatus().equals(MemberStatus.WITHDRAWAL)){
+            throw new MemberException(MemberErrorCode.WITHDRAW_USER);
+        }
+
+    }
+
+    @Transactional
+    public Member registerNewMember(MemberRegisterRequest request){
+
+        validateRegisterMember(request.getEmail(), request.getNickname());
+
+        Member member = Member.builder()
+                .email(request.getEmail())
+                .nickname(request.getNickname())
+                .status(MemberStatus.WAIT)
+                .authType(AuthType.GENERAL)
+                .role(Role.ROLE_MEMBER)
+                .build();
+
+        member.addPassword(passwordEncoder.encode(request.getPassword()));
+        memberRepository.save(member);
+        sendEmail(request.getEmail());
+
+        return member;
+
+
+    }
+
+    private void validateRegisterMember(String email, String nickname){
+
+        if(memberRepository.findByEmail(email).isPresent()){
+            throw new MemberException(MemberErrorCode.SAME_EMAIL_EXISTS);
+        }
+
+        if(memberRepository.findByNickname(nickname).isPresent()){
+            throw new MemberException(MemberErrorCode.SAME_NICKNAME_EXISTS);
+        }
+    }
+
+    public void logoutMember(MemberInfo memberInfo) {
+        refreshTokenRepository.deleteByUsername(memberInfo.getEmail());
+    }
+
+
+
+    @Transactional
+    public void confirmEmail(EmailAuthRequest request) {
+        EmailAuth emailAuth = emailAuthRepository.findValidAuthByEmail(
+                        request.getEmail(),
+                        request.getAuthToken(),
+                        LocalDateTime.now()
+                )
+                .orElseThrow(() -> new EmailAuthException(EmailAuthErrorCode.AUTH_TOKEN_NOT_FOUND));
+        Member member = memberRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND));
+        emailAuth.useToken();
+        member.emailVerifiedSuccess();
+    }
+
+    @Transactional
+    public void sendEmail(String email){
+        EmailAuth emailAuth = emailAuthRepository.save(
+                EmailAuth.builder()
+                        .email(email)
+                        .authToken(UUID.randomUUID().toString())
+                        .expired(false)
+                        .build()
+        );
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailAuth.getEmail());
+        message.setSubject("회원가입 이메일 인증");
+        message.setText("http://localhost:8080/member-auth/confirm-email?email="
+                + emailAuth.getEmail() + "&authToken=" + emailAuth.getAuthToken()
+        );
+
+        javaMailSender.send(message);
+    }
+}
---
+++ backend/src/main/java/com/zerogift/backend/member/web/MemberLoginController.java
@@ -0,0 +1,91 @@
+package com.zerogift.backend.member.web;
+
+import com.zerogift.backend.common.dto.Result;
+import com.zerogift.backend.email.dto.EmailAuthRequest;
+import com.zerogift.backend.member.dto.MemberLoginRequest;
+import com.zerogift.backend.member.dto.MemberRegisterRequest;
+import com.zerogift.backend.member.entity.Member;
+import com.zerogift.backend.member.service.MemberLoginService;
+import com.zerogift.backend.security.dto.MemberInfo;
+import com.zerogift.backend.security.dto.TokenDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Tag(name = "MemberLogin",description = "멤버 로그인 관련 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member-auth")
+public class MemberLoginController {
+
+    private final MemberLoginService memberLoginService;
+
+    @Operation(
+            summary = "로그인", description = "일반 회원 로그인입니다.",
+            tags = {"Member"}
+    )
+
+    @PostMapping("/login")
+    public ResponseEntity<Result<?>> login(
+            @RequestBody @Valid MemberLoginRequest request
+    ){
+
+        TokenDto login = memberLoginService.login(request);
+
+        return ResponseEntity.ok().body(Result.builder()
+                .status(200)
+                .success(true)
+                .data(login)
+                .build());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Result<?>> register(
+            @RequestBody @Valid MemberRegisterRequest request
+    ){
+
+        Member member = memberLoginService.registerNewMember(request);
+
+        return ResponseEntity.ok().body(Result.builder()
+                .status(200)
+                .success(true)
+                .data(member.getEmail())
+                .build());
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Result<?>> logout(@AuthenticationPrincipal MemberInfo memberInfo){
+
+        memberLoginService.logoutMember(memberInfo);
+
+        return ResponseEntity.ok().body(
+                Result.builder()
+                        .status(200)
+                        .success(true)
+                        .data(null)
+                        .build()
+        );
+    }
+
+    @ApiOperation(value = "이메일 인증", notes = "이메일 인증을 진행한다.")
+    @GetMapping("/confirm-email")
+    public ResponseEntity<Result<?>> confirmEmail(
+            @RequestBody @Valid EmailAuthRequest request
+    )
+    {
+        memberLoginService.confirmEmail(request);
+        return ResponseEntity.ok().body(
+                Result.builder()
+                        .status(200)
+                        .success(true)
+                        .data("인증이 완료되었습니다.")
+                        .build()
+        );
+    }
+}
