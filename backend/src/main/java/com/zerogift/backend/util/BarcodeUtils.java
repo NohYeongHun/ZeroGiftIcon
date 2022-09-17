732179b0c8226a1ae6f1516fb140cee12412356d
Shin seongsu <tlstjdtn321@naver.com>
2022-09-18 06:54:32 +0900

    feat: 바코드 유틸 추가

---
+++ backend/src/main/java/com/zerogift/backend/util/BarcodeUtils.java
@@ -0,0 +1,41 @@
+package com.zerogift.backend.util;
+
+import java.awt.image.BufferedImage;
+import lombok.RequiredArgsConstructor;
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeException;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.BarcodeImageHandler;
+import okhttp3.HttpUrl;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BarcodeUtils {
+    private final FileUtil fileUtil;
+
+    private static final String URL = "https://zerogift.p-e.kr/giftBox/giftcon";
+
+    public String barcodeSave(Long giftBoxId, String code) {
+        try {
+            Barcode barcode = BarcodeFactory.createCode128B(makeUrl(giftBoxId, code));
+            barcode.setDrawingText(false);
+            barcode.setBarHeight(100);
+
+            BufferedImage bufferedImage = BarcodeImageHandler.getImage(barcode);
+            return fileUtil.update(bufferedImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("파일 저장중 에러가 발생하였습니다.");
+        }
+    }
+
+    private String makeUrl(Long giftBoxId, String code) {
+        HttpUrl.Builder url = HttpUrl.parse(URL).newBuilder();
+        url.addPathSegment(giftBoxId+"")
+            .addQueryParameter("code", code);
+
+        return url.toString();
+    }
+
+}
