85a4d588f29daff0ea96b8ec16f476a46b421d37
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-18 12:44:27 +0900

    불필요한 temp 삭제

--- backend/src/main/java/com/zerogift/backend/view/controller/ViewHistoryController.java
+++ backend/src/main/java/com/zerogift/backend/view/controller/ViewHistoryController.java
@@ -1,8 +1,5 @@
 package com.zerogift.backend.view.controller;
 
-import com.zerogift.backend.common.dto.Result;
-import com.zerogift.backend.common.exception.code.MemberErrorCode;
-import com.zerogift.backend.common.exception.code.ProductErrorCode;
 import com.zerogift.backend.util.TokenUtil;
 import com.zerogift.backend.view.service.ViewHistoryService;
 import lombok.RequiredArgsConstructor;
@@ -15,21 +12,10 @@ public class ViewHistoryController {
 
     private final ViewHistoryService viewHistoryService;
 
-
-    // --- 테스트 용 ---
-    // 조회수 증가 (임의의 이메일을 RequestBody 로 받아서 사용)
-    @PutMapping("/user/{productId}/viewHistory/Temp")
-    public ResponseEntity<Result<?>> addViewHistoryTemp(@PathVariable Long productId, @RequestHeader("EMAIL") String email) {
-        return viewHistoryService.addViewHistory(email, productId);
-    }
-
     // 조회수 증가
     @PutMapping("/user/{productId}/viewHistory")
     public ResponseEntity<?> addViewHistory(@PathVariable Long productId) {
-        String email = TokenUtil.getAdminEmail();
-        if (email == null) {
-            return ResponseEntity.badRequest().body(MemberErrorCode.MEMBER_NOT_FOUND);
-        }
+        String email = TokenUtil.getAdminEmail() != null ? TokenUtil.getAdminEmail() : TokenUtil.getMemberEmail();
         return viewHistoryService.addViewHistory(email, productId);
     }
 }
