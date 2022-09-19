437e33a7505a52c223e3d78c74fc55584d7ff6c8
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-19 22:54:36 +0900

    Token -> LoginInfo ResponseEntity -> void

--- backend/src/main/java/com/zerogift/backend/view/service/ViewHistoryService.java
+++ backend/src/main/java/com/zerogift/backend/view/service/ViewHistoryService.java
@@ -1,10 +1,9 @@
 package com.zerogift.backend.view.service;
 
-import com.zerogift.backend.common.dto.Result;
-import org.springframework.http.ResponseEntity;
+import com.zerogift.backend.security.dto.LoginInfo;
 
 public interface ViewHistoryService {
 
     // 조회수 증가
-    ResponseEntity<Result<?>> addViewHistory(String email, Long productId);
+    void addViewHistory(LoginInfo loginInfo, Long productId);
 }
