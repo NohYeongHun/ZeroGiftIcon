e25a759a6a292d53c0a1fe8259a209d8912bfa68
Shin seongsu <tlstjdtn321@naver.com>
2022-09-20 05:56:46 +0900

    test: PasswordEncoder mockito 수정

--- backend/src/test/java/com/zerogift/backend/member/service/MemberLoginServiceTest.java
+++ backend/src/test/java/com/zerogift/backend/member/service/MemberLoginServiceTest.java
@@ -1,47 +1,37 @@
 package com.zerogift.backend.member.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.doNothing;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
 import com.zerogift.backend.common.type.AuthType;
-import com.zerogift.backend.config.SecurityConfig;
-import com.zerogift.backend.email.entity.EmailAuth;
-import com.zerogift.backend.email.repository.EmailAuthRepository;
 import com.zerogift.backend.email.service.EmailService;
-import com.zerogift.backend.member.dto.MemberLoginRequest;
 import com.zerogift.backend.member.dto.MemberRegisterRequest;
 import com.zerogift.backend.member.entity.Member;
 import com.zerogift.backend.member.repository.MemberRepository;
 import com.zerogift.backend.member.type.MemberStatus;
 import com.zerogift.backend.security.service.TokenService;
 import com.zerogift.backend.security.type.Role;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.doNothing;
-
-@EnableAutoConfiguration
 @ExtendWith(MockitoExtension.class)
 class MemberLoginServiceTest {
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @Mock
     MemberRepository memberRepository;
@@ -52,49 +42,44 @@ class MemberLoginServiceTest {
     @Mock
     EmailService emailService;
 
-    @Mock
-    PasswordEncoder passwordEncoder;
-
     @InjectMocks
     MemberLoginService memberLoginService;
 
     private Member member;
-
+    private PasswordEncoder encoder;
 
     @BeforeEach
     void setup() {
-        this.passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         member = Member.builder()
-                    .email("email@naver.com")
-                    .nickname("nickname")
-                    .status(MemberStatus.WAIT)
-                    .authType(AuthType.GENERAL)
-                    .role(Role.ROLE_MEMBER)
-                    .build();
+            .email("email@naver.com")
+            .nickname("nickname")
+            .status(MemberStatus.WAIT)
+            .authType(AuthType.GENERAL)
+            .role(Role.ROLE_MEMBER)
+            .build();
     }
 
     @Test
     @DisplayName("회원 등록 - 성공")
-    void testSuccessRegisterMember(){
+    void testSuccessRegisterMember() {
         //given
         String password = "password";
-        String encodingPassword = passwordEncoder.encode(password);
+        String encodingPassword = encoder.encode(password);
         MemberRegisterRequest request = MemberRegisterRequest.builder()
-                .email(member.getEmail())
-                .nickname(member.getNickname())
-                .password(password)
-                .build();
+            .email(member.getEmail())
+            .nickname(member.getNickname())
+            .password(password)
+            .build();
 
         member.addPassword(encodingPassword);
         given(memberRepository.findByEmail(anyString())).
-                willReturn(Optional.empty());
+            willReturn(Optional.empty());
         given(memberRepository.findByNickname(anyString()))
-                .willReturn(Optional.empty());
+            .willReturn(Optional.empty());
         given(memberRepository.save(any()))
-                .willReturn(member);
-
-//        given(passwordEncoder.encode(anyString()))
-//                .willReturn(encodingPassword);
+            .willReturn(member);
+        when(passwordEncoder.encode(any())).thenReturn(encodingPassword);
 
         doNothing().when(emailService).sendEmail(anyString());
 
@@ -102,9 +87,9 @@ class MemberLoginServiceTest {
         Member registerMember = memberLoginService.registerNewMember(request);
 
         //then
-        assertTrue(passwordEncoder.matches(
-                password,
-                registerMember.getPassword()
+        assertTrue(encoder.matches(
+            password,
+            registerMember.getPassword()
         ));
         assertEquals(member.getEmail(), registerMember.getEmail());
         assertEquals(member.getNickname(), registerMember.getNickname());
