1975237be21c6957f4b112f6919fc04f7c863641
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-23 01:39:08 +0900

    선물 받은게 있는지 확인하기 위해 작성

--- backend/src/main/java/com/zerogift/backend/giftBox/repository/GiftBoxRepository.java
+++ backend/src/main/java/com/zerogift/backend/giftBox/repository/GiftBoxRepository.java
@@ -1,8 +1,13 @@
 package com.zerogift.backend.giftBox.repository;
 
 import com.zerogift.backend.giftBox.entity.GiftBox;
+import com.zerogift.backend.member.entity.Member;
+import com.zerogift.backend.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface GiftBoxRepository extends JpaRepository<GiftBox, Long>, GiftBoxRepositoryCustom {
 
+    Optional<GiftBox> findByRecipientMemberAndProduct(Member member, Product product);
 }
