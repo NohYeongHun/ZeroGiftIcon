3b35812f88a496e4c672d6dcea9538c870950153
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-23 01:36:04 +0900

    Review 와 관련된 에러메시지

---
+++ backend/src/main/java/com/zerogift/backend/common/exception/code/ReviewErrorCode.java
@@ -0,0 +1,15 @@
+package com.zerogift.backend.common.exception.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum ReviewErrorCode {
+    REVIEW_NOT_FOUND(" 해당 리뷰를 찾을 수 없습니다. "),
+    ADD_REVIEW_AFTER_USE("선물을 사용 후 리뷰를 작성해 주십시오.");
+
+    private String description;
+}
