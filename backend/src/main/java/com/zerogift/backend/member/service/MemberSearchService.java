ea37c09efe0af0235a16cccbf3a75d0a10dc6bd9
ny <dkzndk1100@naver.com>
2022-09-23 18:24:22 +0900

    feat : 회원 조회용 컨트롤러, 서비스 작성

---
+++ backend/src/main/java/com/zerogift/backend/member/service/MemberSearchService.java
@@ -0,0 +1,28 @@
+package com.zerogift.backend.member.service;
+
+import com.zerogift.backend.common.dto.MyPageableDto;
+import com.zerogift.backend.member.dto.MemberSearchOutputDto;
+import com.zerogift.backend.member.dto.SearchMember;
+import com.zerogift.backend.member.repository.MemberSearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberSearchService {
+
+    private final MemberSearchRepository memberSearchRepository;
+
+
+    @Transactional
+    public Page<MemberSearchOutputDto> searchMemberList(
+            SearchMember searchMember,
+            MyPageableDto myPageableDto) {
+
+        return memberSearchRepository.searchByWhere(
+                searchMember.toCondition(), myPageableDto);
+    }
+
+}
