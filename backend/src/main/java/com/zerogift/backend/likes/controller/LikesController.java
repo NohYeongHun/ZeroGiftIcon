c1f8a61c0c2afb960b21cd9a59e3897fe4a9d0e8
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-29 18:17:25 +0900

    Operation 추가

--- backend/src/main/java/com/zerogift/backend/likes/controller/LikesController.java
+++ backend/src/main/java/com/zerogift/backend/likes/controller/LikesController.java
@@ -4,6 +4,7 @@ import com.zerogift.backend.config.authorization.AuthenticationPrincipal;
 import com.zerogift.backend.likes.service.LikesService;
 import com.zerogift.backend.security.dto.LoginInfo;
 import com.zerogift.backend.util.TokenUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,19 +15,28 @@ public class LikesController {
 
     private final LikesService likesService;
 
-    // 상품에 좋아요 누르기
+    @Operation(
+            summary = "좋아요 누르기", description = "상품에 좋아요 누르기",
+            tags = {"Likes"}
+    )
     @PutMapping("/user/{productId}/likes")
     public ResponseEntity<?> pressLike(@PathVariable Long productId, @AuthenticationPrincipal LoginInfo loginInfo) {
         return likesService.pressLike(loginInfo, productId);
     }
 
-    // 상품에 좋아요 지우기
+    @Operation(
+            summary = "좋아요 취소하기", description = "상품에 좋아요 취소하기",
+            tags = {"Likes"}
+    )
     @DeleteMapping("/user/{productId}/likes")
     public ResponseEntity<?> likeCancel(@PathVariable Long productId, @AuthenticationPrincipal LoginInfo loginInfo) {
         return likesService.likeCancel(loginInfo, productId);
     }
 
-    // 사용자가 좋아요한 리스트 호출
+    @Operation(
+            summary = "회원의 좋아요 리스트", description = "회원이 좋아요 눌렀던 상품들 리스트 호출",
+            tags = {"Likes"}
+    )
     @GetMapping("/user/likes/list")
     public ResponseEntity<?> likeList(@AuthenticationPrincipal LoginInfo loginInfo) {
         return likesService.likeList(loginInfo);
