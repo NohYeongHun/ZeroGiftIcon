e0d9be30f722ce762ca8ea17c7b81d753c8e27ce
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-09-29 13:33:11 +0900

    LoginInfo를 RequestHeader로 잘못받고있었습니다.

--- backend/src/main/java/com/zerogift/backend/review/controller/ReviewController.java
+++ backend/src/main/java/com/zerogift/backend/review/controller/ReviewController.java
@@ -15,14 +15,14 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @PostMapping("/user/{productId}/review")
-    public ResponseEntity<?> addReview(@RequestHeader LoginInfo loginInfo,
+    public ResponseEntity<?> addReview(@AuthenticationPrincipal LoginInfo loginInfo,
                                         @PathVariable Long productId,
                                         @RequestBody ReviewInput reviewInput) {
         return ResponseEntity.ok(reviewService.addReview(loginInfo, productId, reviewInput));
     }
 
     @PatchMapping("/user/{reviewId}/review")
-    public ResponseEntity<?> modifyReview(@RequestHeader LoginInfo loginInfo,
+    public ResponseEntity<?> modifyReview(@AuthenticationPrincipal LoginInfo loginInfo,
                                           @PathVariable Long reviewId,
                                           @RequestBody ReviewInput reviewInput) {
         return ResponseEntity.ok(reviewService.modifyReview(loginInfo, reviewId, reviewInput));
@@ -31,14 +31,14 @@ public class ReviewController {
     private static final String REMOVE_REVIEW_SUCCESS = "리뷰 삭제를 성공하였습니다.";
 
     @DeleteMapping("/user/{reviewId}/review")
-    public ResponseEntity<?> removeReview(@RequestHeader LoginInfo loginInfo,
+    public ResponseEntity<?> removeReview(@AuthenticationPrincipal LoginInfo loginInfo,
                                           @PathVariable Long reviewId) {
         reviewService.removeReview(loginInfo, reviewId);
         return ResponseEntity.ok(REMOVE_REVIEW_SUCCESS);
     }
 
     @GetMapping("/review/user")
-    public ResponseEntity<?> userReviewList(@RequestHeader LoginInfo loginInfo) {
+    public ResponseEntity<?> userReviewList(@AuthenticationPrincipal LoginInfo loginInfo) {
         return ResponseEntity.ok().body(reviewService.userReviewList(loginInfo));
     }
 
