3d690720eea3e8791d82dc3ae5f250b65d9d48b1
ny <dkzndk1100@naver.com>
2022-10-01 16:21:24 +0900

    feat : 회원 결제용 정보 테스트 코드

--- backend/src/test/java/com/zerogift/backend/documentaion/MemberSearchDocumentation.java
+++ backend/src/test/java/com/zerogift/backend/documentaion/MemberSearchDocumentation.java
@@ -1,20 +1,28 @@
 package com.zerogift.backend.documentaion;
 
+import com.zerogift.backend.config.authorization.AuthenticationPrincipalArgumentResolver;
 import com.zerogift.backend.member.service.MemberSearchService;
+import com.zerogift.backend.security.dto.AdminInfo;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
-import static com.zerogift.backend.documentaion.member.MemberSearchStep.member_list_search_documentation;
-import static com.zerogift.backend.documentaion.member.MemberSearchStep.member_list_search_response_create;
+import static com.zerogift.backend.documentaion.member.MemberSearchDetailStep.member_detail_search_documentation;
+import static com.zerogift.backend.documentaion.member.MemberSearchDetailStep.member_detail_search_response_create;
+import static com.zerogift.backend.documentaion.member.MemberSearchListStep.member_list_search_documentation;
+import static com.zerogift.backend.documentaion.member.MemberSearchListStep.member_list_search_response_create;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 public class MemberSearchDocumentation extends Documentation{
 
     @MockBean
     private MemberSearchService memberSearchService;
+
+    @MockBean
+    private AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
 
     @Test
     void memberListSearch(){
@@ -26,8 +34,23 @@ public class MemberSearchDocumentation extends Documentation{
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .queryParam("nickname", "nickname")
                 .queryParam("email","email")
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/member-search/member-list")
+                .then().log().all().extract();
+    }
+
+    @Test
+    void memberSearchDetail(){
+        when(authenticationPrincipalArgumentResolver.resolveArgument(any(), any(), any(), any())).thenReturn(
+                AdminInfo.builder()
+                        .id(1L)
+                        .build());
+        when(memberSearchService.searchMemberDetail(anyLong())).thenReturn(member_detail_search_response_create());
+
+        RestAssured
+                .given(specification).log().all()
+                .filter(member_detail_search_documentation())
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/member-search/member")
                 .then().log().all().extract();
     }
 }
