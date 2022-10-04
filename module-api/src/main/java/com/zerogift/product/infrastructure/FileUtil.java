7d176654b00b353f6993a17d3e5e1562531dd900
DESKTOP-HE6OA6V\양성준 <didtdjwns111@gmail.com>
2022-10-04 22:48:04 +0900

    불필요한 패키지 s3 제거

--- module-api/src/main/java/com/zerogift/product/infrastructure/FileUtil.java
+++ module-api/src/main/java/com/zerogift/product/infrastructure/FileUtil.java
@@ -1,4 +1,4 @@
-package com.zerogift.product.infrastructure.s3;
+package com.zerogift.product.infrastructure;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
